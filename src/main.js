07b1d95f5918c567d7766d5ebc61bd7debd2d0cb
Mathieu Valery <mathieuromainvalery@gmail.com>
2021-03-05 14:24:29 +0100

    add static config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import store from './store';
+import dotenv  from "dotenv"
+
+
+dotenv.config()
 
 Vue.config.productionTip = false
 
